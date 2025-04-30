cd1d6adb90df2292511b9a6ff13c3a228639aca3
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-04-29 21:00:36 -0300

    feat: Add dirty code as example

---
+++ src/dirty/code/Main.java
@@ -0,0 +1,11 @@
+package dirty.code;
+
+public class Main {
+
+	public static void main(String[] args) {
+		R e = new R();
+		
+		e.d();
+	}
+
+}
---
+++ src/dirty/code/R.java
@@ -0,0 +1,12 @@
+package dirty.code;
+
+public class R {
+
+	public void d() {
+		int a = 0;
+		while (a < 10) {
+			System.out.println(a * 3 + 2);
+			a++;
+		}
+	}
+}
